a3784c9ad1deaba610eda1e69851d32ef54c843b
Aleksandr Ivanov <aivanov@oneinc.com>
2022-12-08 11:36:39 -0600

    Add unit tests for 'pnpm import'
    And add small improvements to unit tests code

---
+++ src/Cake.Pnpm.Tests/Import/PnpmImportTests.cs
@@ -0,0 +1,38 @@
+﻿using Cake.Pnpm.Import;
+using NUnit.Framework;
+
+namespace Cake.Pnpm.Tests.Import;
+
+[TestFixture]
+[TestOf(typeof(PnpmImport))]
+public class PnpmImportTests
+{
+    [SetUp]
+    public void Init()
+    {
+        _fixture = new PnpmImportFixture();
+    }
+
+    private PnpmImportFixture _fixture;
+
+    [Test]
+    public void Should_Throw_If_Settings_Are_Null()
+    {
+        // Given
+        _fixture.Settings = null;
+
+        // When
+        // Then
+        Assert.That(_fixture.Run, Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Should_Add_Mandatory_Arguments()
+    {
+        // When
+        var result = _fixture.Run();
+
+        // Then
+        Assert.That(result.Args, Is.EqualTo("import"));
+    }
+}
