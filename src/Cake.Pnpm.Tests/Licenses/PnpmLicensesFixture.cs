a2eb508bc23769f9668ab96b59d952ecb006036c
Aleksandr Ivanov <aivanov@oneinc.com>
2022-12-09 16:11:20 -0600

    Add `pnpm licenses` unit tests

---
+++ src/Cake.Pnpm.Tests/Licenses/PnpmLicensesFixture.cs
@@ -0,0 +1,13 @@
+﻿using Cake.Pnpm.Audit;
+using Cake.Pnpm.Licenses;
+
+namespace Cake.Pnpm.Tests.Licenses;
+
+internal sealed class PnpmLicensesFixture : PnpmFixture<PnpmLicensesSettings>
+{
+    protected override void RunTool()
+    {
+        var tool = new PnpmLicenses(FileSystem, Environment, ProcessRunner, Tools, Log);
+        tool.List(Settings);
+    }
+}
