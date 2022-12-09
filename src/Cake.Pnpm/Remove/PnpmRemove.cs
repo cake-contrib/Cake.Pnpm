bf3c25ae183b5cbca7b0b29397b74a19d3fb213c
Aleksandr Ivanov <aivanov@oneinc.com>
2022-12-09 11:48:29 -0600

    Implement `pnpm remove` command

--- src/Cake.Pnpm/Remove/PnpmRemove.cs
+++ src/Cake.Pnpm/Remove/PnpmRemove.cs
@@ -4,31 +4,33 @@ using Cake.Core.Diagnostics;
 using Cake.Core.IO;
 using Cake.Core.Tooling;
 
-namespace Cake.Pnpm.Add;
+namespace Cake.Pnpm.Remove;
 
 /// <summary>
-///     Installs a package and any packages that it depends on.
+///     Removes packages from `node_modules` and from the project's `package.json`.
+///     Visit https://pnpm.io/7.x/cli/remove for documentation about this command.
 /// </summary>
-public class PnpmAdd : PnpmTool<PnpmAddSettings>
+public class PnpmRemove : PnpmTool<PnpmRemoveSettings>
 {
     /// <summary>
-    ///     Initializes a new instance of the <see cref="PnpmAdd" /> class.
+    ///     Initializes a new instance of the <see cref="PnpmRemove" /> class.
     /// </summary>
     /// <param name="fileSystem">The file system.</param>
     /// <param name="environment">The environment.</param>
     /// <param name="processRunner">The process runner.</param>
     /// <param name="tools">The tool locator.</param>
     /// <param name="log">Cake log instance.</param>
-    public PnpmAdd(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner processRunner,
+    public PnpmRemove(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner processRunner,
         IToolLocator tools, ICakeLog log) : base(fileSystem, environment, processRunner, tools, log)
     {
     }
 
     /// <summary>
-    ///     Installs a package and any packages that it depends on.
+    ///     Removes packages from `node_modules` and from the project's `package.json`.
+    ///     Visit https://pnpm.io/7.x/cli/remove for documentation about this command.
     /// </summary>
     /// <param name="settings">The settings.</param>
-    public void Add(PnpmAddSettings settings)
+    public void Remove(PnpmRemoveSettings settings)
     {
         if (settings == null) throw new ArgumentNullException(nameof(settings));
 
