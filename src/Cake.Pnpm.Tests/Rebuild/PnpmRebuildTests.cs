aff9085b2856b5997d42798b3e8d51f2c81c2d95
Aleksandr Ivanov <aivanov@oneinc.com>
2022-12-13 10:37:31 -0600

    Add `pnpm rebuild` unit tests

---
+++ src/Cake.Pnpm.Tests/Rebuild/PnpmRebuildTests.cs
@@ -0,0 +1,79 @@
+﻿using Cake.Pnpm.Prune;
+using Cake.Pnpm.Rebuild;
+using NUnit.Framework;
+
+namespace Cake.Pnpm.Tests.Rebuild;
+
+[TestFixture]
+[TestOf(typeof(PnpmRebuild))]
+public class PnpmRebuildTests
+{
+    [SetUp]
+    public void Init()
+    {
+        _fixture = new PnpmRebuildFixture();
+    }
+
+    private PnpmRebuildFixture _fixture;
+
+    [Test]
+    public void Should_Throw_If_Settings_Are_Null()
+    {
+        // Given
+        _fixture.Settings = null;
+
+        // When
+        // Then
+        Assert.That(_fixture.Run, Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Should_Add_Mandatory_Arguments()
+    {
+        // When
+        var result = _fixture.Run();
+
+        // Then
+        Assert.That(result.Args, Is.EqualTo("rebuild"));
+    }
+
+    [Test]
+    public void Should_Add_Single_Package_If_Presented()
+    {
+        // Given
+        _fixture.Settings.Packages.Add("foo@bar");
+
+        // When
+        var result = _fixture.Run();
+
+        // Then
+        Assert.That(result.Args, Is.EqualTo("rebuild \"foo@bar\""));
+    }
+
+    [Test]
+    public void Should_Add_Multiple_Package_If_Presented()
+    {
+        // Given
+        _fixture.Settings.Packages.Add("foo@bar");
+        _fixture.Settings.Packages.Add("baz@v1.2.3");
+
+        // When
+        var result = _fixture.Run();
+
+        // Then
+        Assert.That(result.Args, Is.EqualTo("rebuild \"foo@bar\" \"baz@v1.2.3\""));
+    }
+
+    [TestCaseSource(typeof(RebuildTestCaseSource))]
+    public string Should_Add_Parameter_If_Set(PnpmRebuildSettings settings)
+    {
+        // Given
+        _fixture.Settings = settings;
+
+        // When
+        var result = _fixture.Run();
+
+        // Then
+        return result.Args;
+    }
+}
---
+++ src/Cake.Pnpm.Tests/Rebuild/RebuildTestCaseSource.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using Cake.Pnpm.Rebuild;
+using NUnit.Framework;
+
+namespace Cake.Pnpm.Tests.Rebuild;
+
+public class RebuildTestCaseSource : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {
+        yield return new TestCaseData(new PnpmRebuildSettings {AggregateOutput = true}).SetName("--aggregate-output").Returns("rebuild --aggregate-output");
+        yield return new TestCaseData(new PnpmRebuildSettings {Color = false}).SetName("--no-color").Returns("rebuild --no-color");
+        yield return new TestCaseData(new PnpmRebuildSettings {Color = true}).SetName("--color").Returns("rebuild --color");
+        yield return new TestCaseData(new PnpmRebuildSettings {Dir = "C:\\Program Files\\Dir"}).SetName("--dir").Returns("rebuild --dir \"C:\\Program Files\\Dir\"");
+        yield return new TestCaseData(new PnpmRebuildSettings {Pending = true}).SetName("--pending").Returns("rebuild --pending");
+        yield return new TestCaseData(new PnpmRebuildSettings {Recursive = true}).SetName("--recursive").Returns("rebuild --recursive");
+        yield return new TestCaseData(new PnpmRebuildSettings {StoreDir = "/etc/bin"}).SetName("--store-dir").Returns("rebuild --store-dir \"/etc/bin\"");
+        yield return new TestCaseData(new PnpmRebuildSettings {Stream = true}).SetName("--stream").Returns("rebuild --stream");
+        yield return new TestCaseData(new PnpmRebuildSettings {UseStderr = true}).SetName("--use-stderr").Returns("rebuild --use-stderr");
+        yield return new TestCaseData(new PnpmRebuildSettings {WorkspaceRoot = true}).SetName("--workspace-root").Returns("rebuild --workspace-root");
+    }
+}
