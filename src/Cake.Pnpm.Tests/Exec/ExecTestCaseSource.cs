bef18608d9b77e961681bd271b20787793fab0f9
Aleksandr Ivanov <aivanov@oneinc.com>
2022-12-13 11:58:37 -0600

    Add `pnpm exec` unit tests

---
+++ src/Cake.Pnpm.Tests/Exec/ExecTestCaseSource.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using Cake.Pnpm.Exec;
+using NUnit.Framework;
+
+namespace Cake.Pnpm.Tests.Exec;
+
+public class ExecTestCaseSource : IEnumerable
+{
+    public const string StubCommand = "jest";
+    public IEnumerator GetEnumerator()
+    {
+        yield return new TestCaseData(new PnpmExecSettings {Command = StubCommand, Recursive = true}).SetName("--recursive").Returns($"-r exec {StubCommand}");
+        yield return new TestCaseData(new PnpmExecSettings {Command = StubCommand, ShellMode = true}).SetName("--shell-mode").Returns($"-c exec {StubCommand}");
+        yield return new TestCaseData(new PnpmExecSettings {Command = StubCommand, Parallel = true}).SetName("--parallel").Returns($"--parallel exec {StubCommand}");
+    }
+}
---
+++ src/Cake.Pnpm.Tests/Exec/PnpmExecFixture.cs
@@ -0,0 +1,12 @@
+﻿using Cake.Pnpm.Exec;
+
+namespace Cake.Pnpm.Tests.Exec;
+
+internal sealed class PnpmExecFixture : PnpmFixture<PnpmExecSettings>
+{
+    protected override void RunTool()
+    {
+        var tool = new PnpmExec(FileSystem, Environment, ProcessRunner, Tools, Log);
+        tool.Exec(Settings);
+    }
+}
